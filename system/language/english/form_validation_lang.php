7bff2553bfc96712188a22ae4c8f5ecc22944f2d
Gréory Frerot <frerot.g@gmail.com>
2012-04-28 10:57:17 +0200

    Mise a jour de Codeigniter 2.0.3 to 2.1.0

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['required']			= "Le champ %s est requis.";
+$lang['required']			= "The %s field is required.";
 $lang['isset']				= "The %s field must have a value.";
-$lang['valid_email']		= "L'email inséré n'est pas valide";
+$lang['valid_email']		= "The %s field must contain a valid email address.";
 $lang['valid_emails']		= "The %s field must contain all valid email addresses.";
 $lang['valid_url']			= "The %s field must contain a valid URL.";
 $lang['valid_ip']			= "The %s field must contain a valid IP.";
@@ -17,6 +17,7 @@ $lang['is_numeric']			= "The %s field must contain only numeric characters.";
 $lang['integer']			= "The %s field must contain an integer.";
 $lang['regex_match']		= "The %s field is not in the correct format.";
 $lang['matches']			= "The %s field does not match the %s field.";
+$lang['is_unique'] 			= "The %s field must contain a unique value.";
 $lang['is_natural']			= "The %s field must contain only positive numbers.";
 $lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
 $lang['decimal']			= "The %s field must contain a decimal number.";
